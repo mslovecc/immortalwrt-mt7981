6c75c7c870a12339b0febd16dbb67f303dd0055f
mslovecc <33970221+mslovecc@users.noreply.github.com>
2023-09-07 10:47:27 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -13,4 +13,4 @@
 echo srv-host=_vlmcs._tcp.lan,OpenWrt.lan,1688,0,100 >> /etc/dnsmasq.conf
 
 # 解决Kmod相关软件包无法安装问题，刷入系统后使/usr/lib/opkg/status中的hash和安装kmod软件包时出错的hash一致。
-sed -i 's/060d4a88a59ff936e5d09f59b94a0195/b70ee1516753f10c063dd361f74167d4/g' /usr/lib/opkg/status
+# sed -i 's/060d4a88a59ff936e5d09f59b94a0195/b70ee1516753f10c063dd361f74167d4/g' /usr/lib/opkg/status
